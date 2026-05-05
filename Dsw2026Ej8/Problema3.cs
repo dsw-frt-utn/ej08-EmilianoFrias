46dbc3ffcb5dd32192df7de4f402ca48fe5adc0d
Emiliano Frias <emilianofrias10@gmail.com>
2026-05-04 21:44:29 -0300

    Problema N°3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorCopia = originalValue;
+
+            valorCopia++;
+            
+            Product copiaProducto = product;
+
+            copiaProducto.SetDescription("Descripción Modificada");
+
+            return $"{originalValue}-{valorCopia}-{product.Description}";
+        }
+        
+    }
+}
