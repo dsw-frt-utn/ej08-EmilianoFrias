1483a94d05734f8b8ef91d5680ba9714369d683d
Emiliano Frias <emilianofrias10@gmail.com>
2026-05-04 22:08:49 -0300

    Problema N°5

---
+++ Dsw2026Ej8/WholesaleSale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal subtotal) : base(subtotal)
+        {
+        }
+
+        public override decimal CalculateTotal()
+        {
+            return Subtotal * 0.90m;
+        }
+    }
+}
