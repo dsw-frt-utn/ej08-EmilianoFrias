ca40204dc4e71b4796c3fb78a57690039534c2dd
Emiliano Frias <emilianofrias10@gmail.com>
2026-05-04 21:34:58 -0300

    Problema N°2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal totalCalculado = quantity <= 0 ? 0 : quantity * unitPrice;
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = totalCalculado
+            };
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
